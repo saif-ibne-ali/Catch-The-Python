29f31b4bb1b87c23bff4a15140a0788d22753c9f
saif-ibne-ali <si87868@gmail.com>
2022-09-02 08:26:31 +0600

    touple create,print,looping and replace

---
+++ Basics/Tuples.py
@@ -0,0 +1,11 @@
+#a tuple looks just like a list except you use parentheses
+#instead of square brackets.
+_2d = (200,50)
+print(_2d[1])
+for d in _2d:
+    print(d)
+print(_2d)
+
+#replacing
+_2d =(100,25)
+print(_2d)
